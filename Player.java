13da9f0ec5e924ca2f0a0574c17cb9bd2fc320aa
Kobe Jee De Luna <kbdeluna@up.edu.ph>
2018-02-10 15:48:35 +0800

    Added player movements

--- Player.java
+++ Player.java
@@ -2,19 +2,37 @@ public class Player{
     private int currRow;
     private int currCol;
     private Texture texture;
-
+    private String prevValue;
     public Player(int currRow, int currCol){
         this.currRow = currRow;
         this.currCol = currCol;
         this.texture = Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "player_down.png");
+        this.prevValue = "v";
     }
 
     public Player(int currRow, int currCol, Texture tex){
+        this.prevValue = "v";
         this.currRow = currRow;
         this.currCol = currCol;
         this.texture = tex;
     }
 
+
+    public void moveUp(){
+        this.currRow -= 1;
+    }
+
+    public void moveDown(){
+        this.currRow += 1;
+    }
+
+    public void moveLeft(){
+        this.currCol -= 1;
+    }
+
+    public void moveRight(){
+        this.currCol += 1;
+    }
     public int getCurrRow(){
         return this.currRow;
     }
@@ -28,5 +46,11 @@ public class Player{
     }
     public void setTexture(Texture newTexture){
         this.texture = newTexture;
+    }
+    public void setPrevValue(String val){
+        this.prevValue = val;
+    }
+    public String getPrevValue(){
+        return this.prevValue;
     }
 }
--- World.java
+++ World.java
@@ -28,9 +28,115 @@ public class World extends JPanel implements KeyListener{
         this.setLayout(null);
     }
     
-    public void keyPressed(KeyEvent e){}
+    public String up(Player player){
+        return this.worldArray[player.getCurrRow()-1][player.getCurrCol()];
+    }
+
+    public String down(Player player){
+        return this.worldArray[player.getCurrRow()+1][player.getCurrCol()];
+    }
+    public String left(Player player){
+        return this.worldArray[player.getCurrRow()][player.getCurrCol()-1];
+    }
+    public String right(Player player){
+        return this.worldArray[player.getCurrRow()][player.getCurrCol()+1];
+    }
+
+    public void keyPressed(KeyEvent e){
+        switch(e.getKeyCode()){
+            case KeyEvent.VK_UP: 
+                invokeAction(Directions.UP);
+                break;
+            case KeyEvent.VK_DOWN:
+                invokeAction(Directions.DOWN);
+                break;
+            case KeyEvent.VK_LEFT:
+                invokeAction(Directions.LEFT);
+                break;
+            case KeyEvent.VK_RIGHT:
+                invokeAction(Directions.RIGHT);
+                break;
+        }
+        this.repaint();
+    }
     public void keyTyped(KeyEvent e){}
     public void keyReleased(KeyEvent e){}
+
+    private void invokeAction(Directions dir){
+        String nextObject = "";
+        int currPlayerRow = this.player.getCurrRow();
+        int currPlayerCol = this.player.getCurrCol();
+        int nextPlayerRow = currPlayerRow;
+        int nextPlayerCol = currPlayerCol;
+        Directions headed = null;
+        boolean outOfBounds = false;
+        switch(dir){
+            case UP:    
+                this.switchPlayerTexture(Directions.UP);
+                nextPlayerRow -= 1;
+                outOfBounds = nextPlayerRow < 0 ? true : false;
+                if(outOfBounds) return; 
+                nextObject = up(this.player);
+                headed = Directions.UP;
+                break;
+            case DOWN: 
+                this.switchPlayerTexture(Directions.DOWN);
+                nextPlayerRow += 1;
+                outOfBounds = nextPlayerRow > ROWS - 1 ? true : false;
+                System.out.println(outOfBounds);
+                if(outOfBounds) return;
+                nextObject = down(this.player);
+                headed = Directions.DOWN;
+                break;
+            case LEFT:  
+                this.switchPlayerTexture(Directions.LEFT);
+                nextPlayerCol -= 1;
+                outOfBounds = nextPlayerCol < 0 ? true : false;
+                if(outOfBounds) return;
+                nextObject = left(this.player);
+                headed = Directions.LEFT;
+                break;
+            case RIGHT:
+                this.switchPlayerTexture(Directions.RIGHT);
+                nextPlayerCol += 1;
+                outOfBounds = nextPlayerCol > COLS - 1 ? true : false;
+                if(outOfBounds) return;
+                nextObject = right(this.player);
+                headed = Directions.RIGHT;
+                break;
+        }
+
+        if( nextObject.equals(VISITED_TILE)
+            || nextObject.equals(UNVISITED_TILE) )
+        {
+            this.worldArray[currPlayerRow][currPlayerCol] = this.player.getPrevValue();
+            this.player.setPrevValue(VISITED_TILE);
+            this.worldArray[nextPlayerRow][nextPlayerCol] = PLAYER;
+            this.movePlayer(headed);
+        }
+        else if( nextObject.equals(GOAL_TILE) ){
+            this.worldArray[currPlayerRow][currPlayerCol] = this.player.getPrevValue();
+            this.player.setPrevValue(GOAL_TILE);
+            this.worldArray[nextPlayerRow][nextPlayerCol] = PLAYER;
+            this.movePlayer(headed);
+        }
+    }
+
+    private void movePlayer(Directions dir){
+        switch(dir){
+            case UP: 
+                this.player.moveUp();
+                break;
+            case DOWN:
+                this.player.moveDown();
+                break;
+            case LEFT:
+                this.player.moveLeft();
+                break;
+            case RIGHT:
+                this.player.moveRight();
+        }
+    }
     public void readFile(String filename){
         try{
             int i = 0;
@@ -42,7 +148,7 @@ public class World extends JPanel implements KeyListener{
                 lineRead = line.split(" ");
                 for(int j = 0; j < COLS; j++){
                     this.worldArray[i][j] = lineRead[j];
-                    if(this.worldArray[i][j].equals(World.PLAYER)) this.player = new Player(i, j);
+                    if(this.worldArray[i][j].equals(PLAYER)) this.player = new Player(i, j);
                 }
                 i += 1;
             }
@@ -72,8 +178,10 @@ public class World extends JPanel implements KeyListener{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        for(int i = 0; i < World.ROWS; i++){
-            for(int j = 0; j < World.COLS; j++){
+        System.out.println("");
+        for(int i = 0; i < ROWS; i++){
+            for(int j = 0; j < COLS; j++){
+                System.out.print(this.worldArray[i][j] + " ");
                 switch(this.worldArray[i][j]){
                     case WALL: 
                         Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "wall.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
@@ -85,7 +193,15 @@ public class World extends JPanel implements KeyListener{
                         Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "grass.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
                         break;
                     case PLAYER:
-                        Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "grass.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
+                        // Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "grass.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
+                        switch(this.player.getPrevValue()){
+                            case VISITED_TILE:
+                                Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "grass.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
+                                break;
+                            case GOAL_TILE:
+                                Main.textureLoader.getTexture(Texture.DEFAULT_PATH, "water.png").render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
+                                break;
+                        }
                         this.player.getTexture().render(g2d, j * Main.TILE_SIZE, i * Main.TILE_SIZE);
                         break;
                     case GOAL_TILE:
@@ -94,6 +210,7 @@ public class World extends JPanel implements KeyListener{
 
                 }
             }
+                System.out.println("");
         }
     }
 }
